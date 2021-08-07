de758ef299fca77891dc699440bee08502fd76f8
Kyle Cesar <kylecesar@Kyles-MacBook-Air.local>
2021-08-07 15:57:06 -0300

    add cpu

---
+++ src/lib/cpu.h
@@ -0,0 +1,8 @@
+//
+// Created by Kyle Cesar on 07/08/21.
+//
+
+#ifndef KERNEL_CPU_H
+#define KERNEL_CPU_H
+
+#endif //KERNEL_CPU_H
---
+++ src/lib/signal.h
@@ -0,0 +1,8 @@
+//
+// Created by Kyle Cesar on 07/08/21.
+//
+
+#ifndef KERNEL_SIGNAL_H
+#define KERNEL_SIGNAL_H
+
+#endif //KERNEL_SIGNAL_H
